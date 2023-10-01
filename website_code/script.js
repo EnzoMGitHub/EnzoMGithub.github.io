9f06ac9b937ef52b1c61f8237a688a784f5f68fc
EnzoMGitHub <vincenzomonterosso9@gmail.com>
2023-10-01 17:23:36 -0400

    progress

--- website_code/script.js
+++ website_code/script.js
@@ -1,1 +1,8 @@
-console.log("Hola")
+function college(col) {
+    if (col != undefined) {
+        document.getElementById('uni').innerHTML = col + ', Class of 2027'
+    } else {
+        console.error("You did not properly call the college function")
+    }
+}
+college("Lorem Ipsum University")
